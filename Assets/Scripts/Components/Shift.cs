d27af4edbbcb34f193d290960d1102e22f2664a5
Michael Prokofyev <mikeprokofyev@gmail.com>
2019-07-20 23:56:41 +0300

    shift with easings

---
+++ Assets/Scripts/Components/Shift.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[Serializable]
+public struct Shift : IComponentData
+{
+    public float3 startPosition;
+    public float3 endPosition;
+    
+    public float duration;
+    public float progress;
+}
--- Assets/Scripts/Systems/ShiftSystem.cs
+++ Assets/Scripts/Systems/ShiftSystem.cs
@@ -1,57 +1,41 @@
-﻿using Unity.Burst;
+﻿using DG.Tweening;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+using UnityEngine.Networking;
 using static Unity.Mathematics.math;
 
 public class ShiftSystem : ComponentSystem
 {
     private EntityManager entityManager;
     
-    private EntityCommandBufferSystem m_Barrier;
-
     protected override void OnCreate()
     {
         base.OnCreate();
         
         entityManager = World.Active.EntityManager;
-        
-        
-        m_Barrier = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-
-        
-    }
-
-    public void OnDirectionPressed(Vector2 direction)
-    {
-        EntityCommandBuffer commandBuffer = m_Barrier.CreateCommandBuffer();
-        
-        
-        Entities.WithNone<Shifting>().ForEach((Entity entity, ref Translation translation) =>
-        {
-            PostUpdateCommands.SetComponent(entity, new Shifting
-            {
-                direction = direction,
-                distanceLeft = 5f,
-                speed = 10f
-            });
-        });
     }
 
     protected override void OnUpdate()
     {
         if (Root.PlayerInput.directionPressed.HasValue)
         {
-            Entities.WithNone<Shifting>().ForEach((Entity entity) =>
+            float2 direction = Root.PlayerInput.directionPressed.Value;
+            
+            Entities.WithNone<Shift>().ForEach((Entity entity, ref Translation translation) =>
             {
-                PostUpdateCommands.AddComponent(entity, new Shifting
+                float3 endPosition = new float3(translation.Value.x + direction.x, translation.Value.y + direction.y, 0);
+
+                PostUpdateCommands.AddComponent(entity, new Shift
                 {
-                    direction = Root.PlayerInput.directionPressed.Value,
-                    distanceLeft = 10f,
-                    speed = 10f
+                    duration = .3f,
+                    startPosition = translation.Value,
+                    endPosition = endPosition,
+                    progress = 0
                 });
             });
 
@@ -60,25 +44,26 @@ public class ShiftSystem : ComponentSystem
     }
 }
 
+/// <summary>
+/// WHY THE HELL I NEED TO SPLIT SYSTEM INTO 2??
+/// </summary>
 public class ShiftSystem2 : ComponentSystem
 {
     protected override void OnUpdate()
     {
-        Entities.ForEach((Entity entity, ref Shifting shifting, ref Translation translation) =>
+        Entities.ForEach((Entity entity, ref Shift shift, ref Translation translation) =>
         {
-            float2 shiftAmount = shifting.direction * shifting.speed * Time.deltaTime;
-            float3 newTranslation = new float3(translation.Value.x + shiftAmount.x, translation.Value.y + shiftAmount.y, 0);
-            translation.Value = newTranslation;
+            shift.progress += Time.deltaTime / shift.duration;
+
+            float progressTween = Easing.Circular.Out(shift.progress);
             
-            float distanceShifted =  math.length(shiftAmount);
-            float distanceLeft = math.max(0, shifting.distanceLeft - distanceShifted);
-            if (distanceLeft <= 0)
+            float3 currentTranslation = math.lerp(shift.startPosition, shift.endPosition, progressTween);
+
+            translation.Value = currentTranslation;
+            
+            if (shift.progress >= 1)
             {
-                PostUpdateCommands.RemoveComponent<Shifting>(entity);
-            }
-            else
-            {
-                shifting.distanceLeft = distanceLeft;
+                PostUpdateCommands.RemoveComponent<Shift>(entity);
             }
         });
     }
